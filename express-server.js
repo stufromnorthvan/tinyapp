2825fa79eca7d37e3d4c5cc39912067270a14ee8
stufromnorthvan <stumitchell@irsss.ca>
2022-10-03 17:30:27 -0700

    adding form submission for longURL submission, as well as generateRandomString function

--- express-server.js
+++ express-server.js
@@ -4,14 +4,38 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = function() {
+  let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+  let randoString = '';
+  let randoInt = function(max) {
+    return Math.floor(Math.random() * max)
+  }
+  for (let r = 0; r < 5; r++) {
+    randoString += chars[randoInt(63)]
+  }
+  return randoString;
+}
+
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
