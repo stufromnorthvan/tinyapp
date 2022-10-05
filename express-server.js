72df633754c571384dcf4178b19c37d3091f65f8
stufromnorthvan <stumitchell@irsss.ca>
2022-10-04 17:58:26 -0700

    formatting changes

--- express-server.js
+++ express-server.js
@@ -12,37 +12,37 @@ const urlDatabase = {
 };
 
 const generateRandomString = function() {
-  let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+  let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
   let randoString = '';
   let randoInt = function(max) {
-    return Math.floor(Math.random() * max)
-  }
+    return Math.floor(Math.random() * max);
+  };
   for (let r = 0; r < 5; r++) {
-    randoString += chars[randoInt(62)]
+    randoString += chars[randoInt(62)];
   }
   return randoString;
-}
+};
 
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`); 
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
-  delete urlDatabase[id]
-  res.redirect("/urls"); 
+  delete urlDatabase[id];
+  res.redirect("/urls");
 });
 
 app.post("/urls/:id/update", (req, res) => {
   const id = req.params.id;
-  urlDatabase[id] = req.body.update
-  res.redirect(`/urls/${id}`)
+  urlDatabase[id] = req.body.update;
+  res.redirect(`/urls/${id}`);
 });
 
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id]
+  const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
 
